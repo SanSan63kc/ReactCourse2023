84ded9521146b7fa77e6b58cf30aaeba387e5e4c
SanSan63kc <Reagent-163-rw-08@yandex.ru>
2024-07-12 12:47:26 +0400

    урок 41

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function App() {
       text: item.text,
       title: item.title,
       date: new Date(item.date),
-      id: Math.max(...oldItems.map(item => item.id)) + 1
+      id: oldItems.length > 0 ? Math.max(...oldItems.map(item => item.id)) + 1 : 1
     }])
   }
 
@@ -42,7 +42,7 @@ function App() {
       <LeftPanel>
         <Header />
         <JournalAddButton></JournalAddButton>
-        <JournalList items={items}/>
+        <JournalList items={items} />
       </LeftPanel>
       <Body>
         <JournalForm onSubmit={addItem} />
--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -4,19 +4,52 @@ import Button from '../Button/Button'
 
 function JournalForm({ onSubmit }) {
 
+    let [formValidState,setFormValidState] = useState({
+        title: true,
+        post: true,
+        date: true
+    })
+
     let addJournalItem = (e) => {
         e.preventDefault()
         let formData = new FormData(e.target)
         let formProps = Object.fromEntries(formData)
-        onSubmit(formProps)
+        let isFormValid = true
+
+        if (!formProps.title?.trim().length){
+            setFormValidState(state=>({...state, title:false}))
+            isFormValid = false
+        } else {
+            setFormValidState(state=>({...state, title:true}))
+        }
+
+        if (!formProps.post?.trim().length){
+            setFormValidState(state=>({...state, post:false}))
+            isFormValid = false
+        } else {
+            setFormValidState(state=>({...state, post:true}))
+        }
+
+        if (!formProps.date){
+            setFormValidState(state=>({...state, date:false}))
+            isFormValid = false
+        } else {
+            setFormValidState(state=>({...state, date:true}))
+        }
+
+        if (!isFormValid){
+            return 
+        } else  onSubmit(formProps)
+
+        /* onSubmit(formProps) */
     }
 
     return (
         <form className='journal-form' onSubmit={addJournalItem}>
-            <input type="text" name="title" />
-            <input type="date" name="date"></input>
+            <input type="text" name="title" style={{border: formValidState.title ? undefined : "1px solid red"}}/>
+            <input type="date" name="date" style={{border: formValidState.date ? undefined : "1px solid red"}}></input>
             <input type="text" name="tag"  />
-            <textarea name="text" id=""></textarea>
+            <textarea name="post" id="" style={{border: formValidState.post ? undefined : "1px solid red"}}></textarea>
             <Button text={"Сохранить"} />
         </form>
     )
